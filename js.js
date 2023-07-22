916edb6cf1e11af4441f9af382bac4e13bfbe786
Payne6 <75501119+Payne6@users.noreply.github.com>
2023-07-22 21:15:38 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,45 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let heroText = document.getElementsByClassName("hero-text")[0];
-    let heroh1 = document.createElement("h1");
-    heroText.append(heroh1);
 
-    let heroCopy = "CREATING ONLINE CONTENT THAT CUTS THROUGH THE NOISE".split("");
-    var index = 0;
+    // scroll animation of text
+    const boxes = document.querySelectorAll(".h2-split");
 
-    var timer = setInterval(function() {
-        if (index < heroCopy.length) {
-            heroh1.append(heroCopy[index]);
-            index++;
-        } else {
-            clearInterval(timer);
-        }
-  }, 50);
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("appear", entry.isIntersecting);
+
+            if (entry.isIntersecting) {
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0,
+        rootMargin: "-180px",
+    });
+
+
+    boxes.forEach(box => {
+        observer.observe(box);
+    });
+
+
+
+
 });
 
 
-const lefts = document.querySelectorAll(".inner-skill-left");
-const rights = document.querySelectorAll(".inner-skill-right");
-
-window.addEventListener("scroll", checkLeftsRights);
-
-function checkLeftsRights() {
-    let trigger = window.innerHeight / 5 * 4;
-
-    lefts.forEach(left => {
-        leftTop = left.getBoundingClientRect().top;
-        if (leftTop < trigger) {
-            left.classList.add("show");
-        } else {
-            left.classList.remove("show");
-        }
-    });
-
-    rights.forEach(right => {
-        rightTop = right.getBoundingClientRect().top;
-        if (rightTop < trigger) {
-            right.classList.add("show");
-        } else {
-            right.classList.remove("show");
-        }
-    });
-}
